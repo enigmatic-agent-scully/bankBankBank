edce6c3dd989d28c2bf6b5c4aeb975c455d1defd
jinjunzhen <jinjunzhen@yahoo.com>
2020-01-23 21:21:30 -0500

    haha1

--- src/main/java/com/aj/BankMainDriver.java
+++ src/main/java/com/aj/BankMainDriver.java
@@ -1,10 +1,49 @@
 package com.aj;
 
+import java.util.Scanner;
+
 public class BankMainDriver {
+	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int num;
+		do {
+			
+			
+			System.out.println("Welcome to use OnePiece bank service  \n\n");
+			System.out.println("press 1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    for user");
+			System.out.println("press 2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    for employee");
+			System.out.println("press 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    if you are lost");
+			num = scan.nextInt();
+			
+			
+			switch (num) {
+			case 1:
+				UserEnterance user1 = new UserEnterance();
+				user1.UserMenu1();
+				break;
+			case 2:
+				EmployeeEnterance employee1 = new EmployeeEnterance();
+				employee1.EmployeeMenu();
+				break;
+			case 3:
+				System.out.println("shut up");
+				break;
+			default:
+				System.out.println("bye bye");
 
+			}
+		} while ( num==1 || num==2 || num ==3);
+		
+		
+		
+		
+		
 	}
+		
+
+		
+	
 
 }
---
+++ src/main/java/com/aj/playWithIt.java
@@ -0,0 +1,10 @@
+package com.aj;
+
+public class playWithIt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
