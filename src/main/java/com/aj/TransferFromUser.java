eff1539f6bd3e7860a247279f6a297cd9594eec9
jinjunzhen <jinjunzhen@yahoo.com>
2020-01-27 14:11:18 -0500

    merge mondy 2pm

--- src/main/java/com/aj/TransferFromUser.java
+++ src/main/java/com/aj/TransferFromUser.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TransferFromUser {
-	
+
 	public static Scanner scan = new Scanner(System.in);
 	public static ArrayList<User> users = new ArrayList<User>();
 	public static ArrayList<Employee> employees = new ArrayList<Employee>();
@@ -17,43 +17,95 @@ public class TransferFromUser {
 	static long accountID;
 	static int num;
 	static boolean flag, yesNoTemp;
-	
+
 	public void transferMenu(int accountIndex) {
-		
+		toLoad();
+		int targetIndex;
+		String singJoint;
+
+
 		do {
+			targetIndex = -1;
 			flag = true;
-			
+
 			System.out.println("Enter the accountID you want to transfer to ");
 			try {
 				accountID = Long.parseLong(scan.nextLine());
 			} catch (NumberFormatException ex) {
 				accountID = -1;
 			}
+
+			targetIndex = checkAccountID(accountID);
 			
-			checkAccountID(accountID);
+			if (targetIndex == -1) {
+				System.out.println(" account not found ");
+				flag = false;
+			} else {
+				System.out.println("do u want to transfer money to " + users.get(targetIndex).getName1() + "? (Y/N");
+				
+				try {
+					singJoint = scan.nextLine().toUpperCase();
+				} catch (NumberFormatException ex) {
+					singJoint = "no";
+				}
+				
+				if (singJoint.contentEquals("Y") || singJoint.contentEquals("YES")) {
+					yesNoTemp = true;
+					flag = false;
+					
+				} else {
+					yesNoTemp = false;
+					flag = true;
+					return;
+				}
+			}
+			
+			System.out.println("how much u want to tansfer? ");
+			
+			double money;
+			
+			try {
+				money = Double.parseDouble(scan.nextLine());
+				System.out.println("money is "+ money);
+			} catch (NumberFormatException ex) {
+				money = 0;
+			}
+			if (money < 0) {
+				money = 0;
+			}
 			
 			
-			
-			
-			
+			double balance;
+			balance = users.get(accountIndex).getChecking();
+			balance -= money;
+			users.get(accountIndex).setChecking(balance);
+			balance = users.get(targetIndex).getChecking();
+			balance += money;
+			users.get(targetIndex).setChecking(balance);
+			toSave();
 			
 		} while(flag);
-		
 
 	}
-	
-public static boolean checkAccountID(long accountID) {
-	
-	for (int i=0; i< users.size(); i++) {
-		if (users.get(i).getAccountID() == accountID) {
-			return true;
+
+	public static int checkAccountID(long accountID) {
+
+		for (int i=0; i< users.size(); i++) {
+			if ( (users.get(i).getAccountID() == accountID) && (users.get(i).isCanceleled()==false) ) {
+				return i;
+			}
 		}
+		return -1;
 	}
-	return false;
-}
+
 	
-public static void toLoad() {
-		
+	
+	
+	
+	
+	
+	public static void toLoad() {
+
 		try {
 
 			// Reading the object from a file
@@ -64,7 +116,7 @@ public static void toLoad() {
 
 			employees = (ArrayList<Employee>) in.readObject();
 			users = (ArrayList<User>) in.readObject();
-			
+
 
 			in.close();
 			file.close();
@@ -80,26 +132,26 @@ public static void toLoad() {
 		}
 	}
 
-public static void toSave(){
-	
-	try {
-		// Saving of object in a file
-		FileOutputStream file = new FileOutputStream(filename);
-		ObjectOutputStream out = new ObjectOutputStream(file);
+	public static void toSave(){
 
-		// Method for serialization of object
-		out.writeObject(employees);
-		out.writeObject(users);
+		try {
+			// Saving of object in a file
+			FileOutputStream file = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(file);
 
-		out.close();
-		file.close();
+			// Method for serialization of object
+			out.writeObject(employees);
+			out.writeObject(users);
 
-		// if create successful jump back to employee menu
+			out.close();
+			file.close();
 
-	} catch (IOException ex) {
-		System.out.println("IOException is caught");
+			// if create successful jump back to employee menu
+
+		} catch (IOException ex) {
+			System.out.println("IOException is caught");
+		}
+
 	}
-	
-}
 
 }
